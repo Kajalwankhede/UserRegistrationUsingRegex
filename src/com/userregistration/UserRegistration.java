8b12fa51730506fcc8e523f242038f71a95a40bc
Kajalwankhede <kajuwankhede1510@gmail.com>
2022-11-17 20:05:12 +0530

    UC2- User need to enter a valid last name

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     ArrayList<String> list = new ArrayList<>();
     private void isValidFirstName() {
+        System.out.println("Checking Valid First Name:");
         list.add("Jonny");
         list.add("Lio");
         list.add("Miaa");
@@ -16,10 +17,24 @@ public class UserRegistration {
             System.out.println("The first name is : ");
             System.out.println(list.get(i) + " -> " + matcher.matches());
         }}
+    private void isValidSecondName() {
+        System.out.println(" Checking Valid Last Name:");
+        list.add("Disuza");
+        list.add("Mirza");
+        list.add("Hud");
+        list.add("Dcruz");
+        String regex = "[A-Z]{1}[a-z]{3,}";
+        Pattern pattern = Pattern.compile(regex);
+        for (int i = 0; i < list.size(); i++) {
+            Matcher matcher = pattern.matcher(list.get(i));
+            System.out.println("The Last name is : ");
+            System.out.println(list.get(i) + " -> " + matcher.matches());
+        }}
     public static void main(String[] args) {
         System.out.println("User Registration using Regex");
         UserRegistration user = new UserRegistration();
-        user.isValidFirstName();
+        //user.isValidFirstName();
+        user.isValidSecondName();
     }
 
     }
